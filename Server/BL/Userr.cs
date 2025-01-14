bcd44991ac97d0b5ae77359b8ea3469ad123508b
OshriBuskila <oshrib297@gmail.com>
2025-01-14 21:40:31 +0200

    Initial commit

---
+++ Server/BL/Userr.cs
@@ -0,0 +1,62 @@
+﻿
+namespace GamesStore.Models
+{
+    public class Userr
+    {
+        int id;
+        string name;
+        string email;
+        string password;
+
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+
+        public Userr() { }
+        public static int Register(string newName,string newEmail, string newPassword)
+        {
+            DBservices dbServices = new DBservices();
+
+            try
+            {
+                int result = dbServices.Register(newName, newEmail, newPassword);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error registering user: {ex.Message}");
+                return 0;
+            }
+        }
+
+       
+        public  Userr Login(string email, string password)
+        {
+            DBservices dbServices = new DBservices();
+            Userr userid = dbServices.userLogin(email, password);
+            return userid; 
+        }
+        public static int UpdateName(int currentID, string newName)
+        { 
+            DBservices dbServices = new DBservices();
+            int result = dbServices.UpdateName(currentID, newName);
+            return result;
+            
+            
+        }
+        public static int UpdatePass(int currentID, string newPass)
+        {
+            DBservices dbServices = new DBservices();
+            int result = dbServices.UpdatePass(currentID, newPass);
+            return result;
+        }
+        public static Userr UpdateUser(Userr changesForUser)
+        {
+            DBservices dBservice = new DBservices();
+            return dBservice.UpdateUser(changesForUser);
+        }
+
+    }
+}
---
+++ Server/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using GamesStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GamesStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+
+        [HttpPost("Register")]
+        public int Register([FromBody] Userr newUser)
+        {
+
+            int result = Userr.Register(newUser.Name, newUser.Email, newUser.Password);
+
+            return result;
+
+        }
+
+        [HttpPost("Login")]
+        public Userr Login([FromBody] Userr userToLogin)
+        {
+            return userToLogin.Login(userToLogin.Email, userToLogin.Password);
+
+        }
+
+        //[HttpPost("UpdateName")]
+        //public int UpdateName(int currentID,string newName)
+        //{
+        //    int result = Userr.UpdateName(currentID, newName);
+        //    return result;
+        //}
+        //[HttpPost("UpdatePassword")]
+        //public int UpdatePass(int currentID, string newPass)
+        //{
+        //    int result = Userr.UpdatePass(currentID, newPass);
+        //    return result;
+        //}
+        [HttpPut("UpdateUser")]
+        public Userr UpdateUser([FromBody]Userr changesForUser)
+        {
+            return Userr.UpdateUser(changesForUser);
+        }
+    }
+}
